ad5d5c3150fc66b5dd8f23fa5e3c06c8d73d9b90
Ahmet Furkan <ahmetfunlu@outlook.com>
2022-04-03 21:40:47 +0300

    The Code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
 public class App {
+
+    //https://www.programiz.com/dsa/insertion-sort
+    static void insertionSort(int array[]) {
+        int size = array.length;
+
+        for (int step = 1; step < size; step++) {
+            int key = array[step];
+            int j = step - 1;
+
+            // Compare key with each element on the left of it until an element smaller than
+            // it is found.
+            // For descending order, change key<array[j] to key>array[j].
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+            }
+
+            // Place key at after the element just smaller than it.
+            array[j + 1] = key;
+        }
+    }
+
+    static void PrintArray(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] input = { 80, 20, 70, 40 };
+        insertionSort(input);
+        PrintArray(input);
     }
 }
